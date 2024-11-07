3b3067a6faec3bd35d92790fbc4ef60040124ea9
rafaelcmartin <rafael.cmartin@gmail.com>
2024-11-07 18:38:07 -0300

    inicial commit

---
+++ src/components/particles/Data.tsx
@@ -0,0 +1,206 @@
+export const NavLinks = [
+    {
+        name: "Home",
+        url: "/"
+    },
+    {
+        name: "Sobre Nós",
+        url: "/about"
+    },
+    {
+        name: "Agendamento",
+        url: "/schedule"
+    },
+    {
+        name: "Galeria",
+        url: "/gallery"
+    },
+    {
+        name: "Novidades",
+        url: "/news"
+    },
+    {
+        name: "Contato",
+        url: "/contact"
+    },
+];
+
+
+
+export const HeroTexts = [
+    {
+        Heading: "Liberte sua força interior!",
+        Paragraph: "Bem-vindo ao GymNexT, onde o condicionamento físico encontra a diversão! Prepare-se para liberar sua força interior e transformar sua vida!",
+        Button: "Junte-se Agora!"
+    },
+    {
+        Heading: "Novos patamares de condicionamento físico!",
+        Paragraph: "Supere seus limites e alcance a grandeza em nossa academia! Junte-se à nossa comunidade de apoio e embarque em uma jornada de autoaperfeiçoamento e capacitação!",
+        Button: "Inscreva-se hoje mesmo!"
+    },
+    {
+        Heading: "Fitness é divertido conosco!",
+        Paragraph: "Na nossa academia, o condicionamento físico não é apenas uma rotina; é uma aventura divertida! Junte-se a nós nesta expedição fitness, onde diversão e resultados andam de mãos dadas!",
+        Button: "Inscreva-se agora mesmo!"
+    }
+]
+
+
+export const AboutTexts = {
+    firstText: "Sobre GymNexT",
+    secondText: "Bem-vindo",
+    caption: "Bem-vindo ao GymNexT, onde o fitness encontra a comunidade e a transformação acontece!",
+    paragraph1: "Na GymNexT, somos mais do que apenas uma academia; somos uma família de fitness apaixonada e solidária, dedicada a ajudá-lo a atingir seus objetivos de saúde e bem-estar. Nossa missão é criar um ambiente positivo e fortalecedor que inspire você a se desafiar, adotar um estilo de vida saudável e descobrir a melhor versão de si mesmo.",
+}
+
+export const OfferTexts = {
+    firstText: "O que fazemos",
+    secondText: "Toda a nossa oferta",
+    list: [
+        {
+            listCaption: "Programas para perder peso",
+            text: "Nossa abordagem personalizada, orientação especializada e resultados comprovados ajudarão você a atingir seus objetivos de condicionamento físico. Dê hoje o primeiro passo em direção a um corpo e estilo de vida transformados.",
+        },
+        {
+            listCaption: "Programas de musculação",
+            text: "Nossos treinadores especializados irão guiá-lo através de treinos personalizados, ajudando você a atingir seus objetivos de musculação com eficiência e segurança.",
+        }
+        , {
+            listCaption: "Diferentes classes especiais",
+            text: "Leve o seu treino a novos patamares com a nossa aula especial alta e intensa, onde você ultrapassará seus limites e alcançará resultados de condicionamento físico notáveis.",
+        }
+    ],
+}
+
+export const MembershipPlans = {
+    firstText: "Tablea de Preços",
+    secondText: "Planos",
+    cards: [
+        {
+            amount: 8,
+            duration: "Diário",
+            caption: "Treinamento de um dia",
+            benefits: ["Acesso único a todos os clubes", "Treinador de grupo", "Reservar aula de grupo", "Orientação de fitness"]
+        },
+        {
+            amount: 49,
+            duration: "Mensal",
+            caption: "12 Meses",
+            benefits: ["Aulas em grupo", "Discutir objetivos de condicionamento físico", "Treinador de grupo", "Orientação de condicionamento físico"]
+        },
+        {
+            amount: 65,
+            duration: "Mensal",
+            caption: "Pagamento todos os meses",
+            benefits: ["Aulas em grupo", "Discutir objetivos de condicionamento físico", "Treinador de grupo", "Orientação de condicionamento físico"]
+        }
+    ]
+}
+
+export const ContactTexts = {
+    firstText: "Contato",
+    phone: "(+55)11-99999-0000",
+    paragraph: "Em nossa academia, você pode entrar em contato com qualquer um de nossos treinadores para agendar uma sessão. Os serviços ao domicílio também estão no âmbito dos nossos serviços. A sua forma física é a nossa preocupação! Saúde é riqueza!",
+    button: "Agente uma visita"
+}
+
+export const TestimonialTexts = {
+    firstText: "Alguns dos nossos clientes",
+    secondText: "O que os clientes dizem",
+    feedBacks: [
+        {
+            text: "Sou membro há seis meses, e a comunidade de apoio da academia e os treinadores experientes me ajudaram a superar meus objetivos de condicionamento físico!",
+            person: "Darrell Murray",
+            type: "Cliente"
+        },
+        {
+            text: "As aulas de Yoga transformaram a minha vida; Sinto-me mais centrado, forte e tranquilo cada vez que piso no tatame.",
+            person: "Derrick Rodriquez",
+            type: "Cliente"
+        },
+        {
+            text: "Participar da Classe Especial foi uma virada de jogo! É intenso, mas os resultados são incríveis. Altamente recomendado!",
+            person: "Collins Thompson",
+            type: "Cliente"
+        }
+    ],
+}
+
+export const BlogTexts = {
+    firstText: "Para mais informações",
+    secondText: "Últimas novidades",
+    blogNews: [
+        {
+            caption: "Yoga",
+            title: "Suas auto-realizações desaparecem rapidamente",
+            paragraph: "Descubra o poder transformador do yoga enquanto exploramos seus inúmeros benefícios físicos e mentais. Desde maior flexibilidade e força até redução do estresse e maior atenção plena, nossas aulas de ioga atendem a praticantes de todos os níveis. Junte-se a nós no tatame e experimente a harmonia entre mente, corpo e alma.",
+            time: "1m",
+            author: "Ralph Cruz",
+            comments: "21"
+        },
+        {
+            caption: "Treinadores e equipamentos",
+            title: "Pequenas coisas fazem a diferença",
+            paragraph: "Em nossa academia, temos orgulho de nossa equipe excepcional de treinadores especializados, apaixonados por orientá-lo em sua jornada de fitness. Com seu conhecimento e abordagem personalizada, você receberá o suporte necessário para atingir seus objetivos de condicionamento físico de maneira eficaz. Além disso, oferecemos equipamentos de última geração projetados para elevar seus treinos e proporcionar uma experiência de condicionamento físico holística como nenhuma outra.",
+            time: "15m",
+            author: "Bruce Charles",
+            comments: "45"
+        },
+        {
+            caption: "Procrastination",
+            title: "A hora é agora",
+            paragraph: "A procrastinação pode ser um obstáculo comum no caminho para alcançar nossos objetivos. Neste blog, nos aprofundamos em estratégias eficazes para superar a procrastinação e aumentar a produtividade. Desde técnicas de gerenciamento de tempo até permanecer motivado, estamos aqui para ajudá-lo a assumir o controle de seus objetivos e a abraçar uma vida mais focada e gratificante. É hora de aproveitar o dia e tornar seus sonhos realidade!",
+            time: "19m",
+            author: "Steve Wagner",
+            comments: "55"
+        }
+    ]
+}
+
+export const FooterTexts = {
+    underLogoText: "Somos mais do que apenas uma academia; somos uma família de fitness apaixonada e solidária, dedicada a ajudá-lo a atingir seus objetivos de saúde e bem-estar. Nossa missão é criar um ambiente positivo e fortalecedor que inspire você a se desafiar, adotar um estilo de vida saudável e descobrir a melhor versão de si mesmo.",
+    quickLinks: {
+        caption: "Links rápidos",
+        links: [
+            {
+                name: "Home",
+                url: "/"
+            },
+            {
+                name: "Sobre nós",
+                url: "/about"
+            },
+            {
+                name: "Angendamento",
+                url: "/schedule"
+            },
+            {
+                name: "Galeria",
+                url: "/gallery"
+            },
+            {
+                name: "Novidades",
+                url: "/news"
+            },
+            {
+                name: "Contato",
+                url: "/contact"
+            },
+        ]
+    },
+    contacts: {
+        caption: "Contato",
+        names: [
+            {
+                name: "Rua das amoras, 1",
+            },
+            {
+                name: "gym_next@example.com.br",
+            },
+            {
+                name: "+5511999999999",
+            }
+        ]
+    },
+    copyright: "Copyright 2024. Todos os direitos reservados"
+}
